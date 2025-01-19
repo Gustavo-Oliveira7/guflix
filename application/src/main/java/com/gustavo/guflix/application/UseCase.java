a4a0411767c6042af144a64e5be6c41645ba2564
Gustavo-Oliveira <gustavo.h.oliveira7531@gmail.com>
2025-01-19 20:12:08 -0300

    category test created

--- application/src/main/java/com/gustavo/guflix/application/UseCase.java
+++ application/src/main/java/com/gustavo/guflix/application/UseCase.java
@@ -1,7 +1,5 @@
 package com.gustavo.guflix.application;
 
-import com.gustavo.guflix.domain.Category;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class UseCase {
