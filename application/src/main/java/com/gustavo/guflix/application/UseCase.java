e4843f4b8de4495952c0beea4e92300f5cea51c1
Gustavo-Oliveira <gustavo.h.oliveira7531@gmail.com>
2025-01-22 10:08:07 -0300

    creating id as a value object

--- application/src/main/java/com/gustavo/guflix/application/UseCase.java
+++ application/src/main/java/com/gustavo/guflix/application/UseCase.java
@@ -1,5 +1,7 @@
 package com.gustavo.guflix.application;
 
+import com.gustavo.guflix.domain.category.Category;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class UseCase {
